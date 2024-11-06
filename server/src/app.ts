9104d70698e06bdb8d84a52db00a6807e4e32685
Abhishek Singh <meabhisingh25@gmail.com>
2024-11-07 03:41:30 +0530

    Final

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
-import morgan from "morgan";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config({ path: "./.env" });
 
